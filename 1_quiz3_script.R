b9424ba41ffdc22ba3bce6e520b31c0d5f1a0758
Alyssa Payne <apayne03@uoguelph.ca>
2016-11-01 13:46:05 -0400

    Make commit.

--- 1_quiz3_script.R
+++ 1_quiz3_script.R
@@ -27,20 +27,31 @@ bfi2_women <- as.data.frame(bfi2_women)
 cocor(~A1+E1|A1+E1, data=list(bfi2_men, bfi2_women))
 
 # Question 4 determine correlation between rating-raises and rating-critical correlation
-r.jk <- .59
-r.jh <- .16
-r.kh <- .19
+r.jk <- .59 
+r.jh <- .16 
+r.kh <- .38 
 n <- 30
 
 cocor.dep.groups.overlap(r.jk, r.jh, r.kh, n)
 
-## determine correlation between rating-complaint and raises-critical correlation
+
+# Question 5 determine correlation between rating-complaint and raises-critical correlation
 r.jk <- .59
-r.hm <- .38
+r.hm <- .83
 r.jh <- .16
-r.jm <- .16
-r.kh <- .19
+r.jm <- .67
+r.kh <- .38
 r.km <- .19
 n <- 30
 
 cocor.dep.groups.nonoverlap(r.jk, r.hm, r.jh, r.jm, r.kh, r.km, n)
+
+
+# Question 6
+
+r1.jk <- -.24
+r2.hm <- .03
+n1=30
+n2=3000
+
+cocor.indep.groups(r1.jk, r2.hm, n1, n2)
